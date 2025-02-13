df7aebee2b165f0157a2a8707445d8aabe96a64e
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-02-13 09:31:49 -0500

    changed iteration to using Substring method, will be able to use this in a function with inputted dna and kmer

--- main/Program.cs
+++ main/Program.cs
@@ -1,40 +1,13 @@
-﻿using System;
+﻿Console.Clear();
 
+string vibrioCholeraeSegment = "atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc";
+int dnaLength = vibrioCholeraeSegment.Length;
 
-class Program {
+int count = 0;
+int kmer = 9;
 
-    static void Main(string[] args)
-    {
-        Console.Clear();
-
-        string vibrioCholeraeSegment = "atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc";
-        int kmer = 9;
-        
-        //creates list of segments from dna with length of kmer, iterating over the dna in groups of nine
-        int segmentAmount = vibrioCholeraeSegment.Length - kmer - 1;
-        List<string> segmentList = new List<string>();
-
-        for (int x = 0; x < segmentAmount; x++)
-        {
-            
-            string segment = "";
-            for (int y = x; y < x + 9; y++ )
-            {
-                segment = segment + vibrioCholeraeSegment[y].ToString();
-            }
-            segmentList.Add(segment);
-        }
-        
-        //iterates through segmentList and counts how many times that segments repeat
-        var countedSegmentList = new List<Tuple<string,int>>();
-        int z = 0;
-        foreach (string dnaSegment in segmentList)
-        {
-            countedSegmentList.Add(new Tuple<string, int>(dnaSegment, z));
-        }
-
-    }
+while (count < dnaLength - kmer + 1)
+{
+    Console.WriteLine(vibrioCholeraeSegment.Substring(count, kmer));
+    count++;
 }
-
-
-
