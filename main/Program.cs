7b4589f55dd0fd866f52d3f3c8317d129024bb93
robbeattyprogramming <robbeattyprogramming@gmail.com>
2025-02-19 14:14:44 -0500

    added 2D countedKmerList that adds kmer segments if they are not already in the array

--- main/Program.cs
+++ main/Program.cs
@@ -1,13 +1,45 @@
-﻿Console.Clear();
-
-string vibrioCholeraeSegment = "atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc";
+﻿string vibrioCholeraeSegment = "atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc";
 int dnaLength = vibrioCholeraeSegment.Length;
 
 int count = 0;
 int kmer = 9;
 
+List<string> fullKmerList = new List<string>();
+
+
 while (count < dnaLength - kmer + 1)
 {
-    Console.WriteLine(vibrioCholeraeSegment.Substring(count, kmer));
+    //Console.WriteLine(vibrioCholeraeSegment.Substring(count, kmer));
+    fullKmerList.Add(vibrioCholeraeSegment.Substring(count, kmer));
+
     count++;
+}
+
+int indexCount = (dnaLength / kmer);
+int test = (dnaLength / kmer) + 30;
+string[,] countedKmerList = new string[test, 2];
+
+
+for (int i = 0; i < indexCount; i++)
+{
+    bool containsSegment = false; 
+
+    for (int y = 0; y < indexCount; y++)
+    {
+        if (countedKmerList[y, 0] == fullKmerList[i])
+        {
+            containsSegment = true;
+        }
+    }
+
+    if (containsSegment == false)
+    {
+        countedKmerList[i, 0] = fullKmerList[i];
+    } 
+}
+
+//Test run
+for(int z = 0; z < indexCount; z++)
+{
+    Console.WriteLine(countedKmerList[z, 0]);
 }
